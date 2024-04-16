2e0791e5996652741c66c5efebef7b1c523b7fcc
augustkoskinen <augustkoskinen@gmail.com>
2024-04-16 14:47:44 -0400

    Better movement, but players are backwards for some reason

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Numerics;
-//using System.Numerics;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    [SerializeField] private float MOVEMENT_SPEED = 7f;
-    //[SerializeField] private float ROTATION_SPEED = 500f;
-    [SerializeField] private float POS_RANGE = 5f;
-    [SerializeField] private float sensX;
-    [SerializeField] private float sensY;
-    [SerializeField] private float groundDrag;
-    [SerializeField] private float gravityStrength;
-    [SerializeField] private int playerHeight;
+    [SerializeField] private Transform playerRoot;
+    [SerializeField] private Transform playerCam;
+    [SerializeField] private float POS_RANGE;
 
-    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float camSense;
 
-    [SerializeField] private GameObject playerCam;
-    [SerializeField] private Transform tf;
-
-    float xRot;
-    float yRot;
-
-    private Vector3 gravity = new Vector3(0, 0, 0);
+    CharacterController controller;
+    [SerializeField] private float airspeed;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float gravity;
+    [SerializeField] private Transform feet;
+    bool grounded;
+    float jumpcount;
+    Vector3 velocity;
+    
+    float rotX;
+    float rotY;
 
     public override void OnNetworkSpawn()
     {
@@ -36,41 +34,56 @@ public class PlayerMovement : NetworkBehaviour
         if(!IsOwner)
         {
             playerCam.GetComponent<Camera>().enabled = false;
+            return;
         }
         Cursor.lockState = CursorLockMode.Locked;
+
+        jumpcount = 0;
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (!IsOwner) return;
-
-        bool newgrounded = Physics.Raycast(transform.position, Vector3.down, .2f, whatIsGround);
-        while(Physics.Raycast(transform.position, Vector3.down, .05f, whatIsGround)) transform.Translate(new Vector3(0, .001f, 0), Space.World);
-        transform.Translate(new Vector3(0, -.001f, 0), Space.World);
-        bool grounded = newgrounded;
-
-        if (grounded) {
-            gravity.y = 0;
-        } else {
-            gravity.y -= gravityStrength;
-        }
+        if(!IsOwner) return;
+        controller.Move(velocity*Time.deltaTime);
+        jumpcount -= Time.deltaTime;
 
         float horizontalin = Input.GetAxis("Horizontal");
         float verticalin = Input.GetAxis("Vertical");
         float mouseX = Input.GetAxisRaw("Mouse X");
         float mouseY = Input.GetAxisRaw("Mouse Y");
+        
+        //cam move
+        rotY -= mouseX*camSense;
+        rotX = Mathf.Clamp(rotX-mouseY*camSense, -90f, 90f);
 
-        Vector3 movement = transform.forward * -verticalin + transform.right * -horizontalin + gravity;//jumpVect
+        playerRoot.rotation = Quaternion.Euler(0, -rotY, 0);
+        playerCam.localRotation = Quaternion.Euler(rotX, 0, 0);
 
-        transform.Translate(movement * MOVEMENT_SPEED * Time.deltaTime, Space.World);
+        //player move
+        Vector3 movevect = playerRoot.forward * verticalin + playerRoot.right * horizontalin;
 
-        yRot += mouseX*sensX;
-        xRot += mouseY*sensY;
+        controller.Move(movevect*(grounded ? speed : airspeed)*Time.deltaTime);
 
-        xRot = Mathf.Clamp(xRot, -90f, 90f);
+        grounded = Physics.Raycast(feet.position, feet.TransformDirection(Vector3.down),.15f);
 
-        transform.rotation = Quaternion.Euler(0, yRot, 0);
-        tf.rotation = Quaternion.Euler(xRot, yRot, 0);
+        if(grounded&&jumpcount<=0) {
+            velocity = new Vector3(0,-3,0);
+        } else {
+            velocity -= Vector3.up*gravity*Time.deltaTime;
+            if(velocity.y < -gravity*.75f) {
+                velocity.y = -gravity*.75f;
+            }
+        }
+
+        if(Input.GetKeyDown("space")) Jump();
+    }
+
+    void Jump() {
+        if(grounded) {
+            jumpcount = .5f;
+            velocity.y = Mathf.Sqrt(2f*jumpHeight*gravity);
+        }
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -79,68 +92,3 @@ public class PlayerMovement : NetworkBehaviour
         transform.position = new Vector3(Random.Range(POS_RANGE, -POS_RANGE), 2, Random.Range(POS_RANGE, -POS_RANGE));
     }
 }
-
-/*
- * // Some stupid rigidbody based movement by Dani
-
-using System;
-using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour {
-    private Rigidbody rb;
-    [SerializeField] private float speed;
-    [SerializeField] private float groundDrag;
-    [SerializeField] private float gravityStrength; 
-    [SerializeField] private int playerHeight;
-    [SerializeField] private Transform tf;
-    [SerializeField] private LayerMask whatIsGround;
-
-
-    private float horizontalInput;
-    private float verticleInput;
-
-    private Vector3 movement;
-    private bool grounded = false;
-    private float maxSlopeAngle = 45f;
-    private Vector3 g;
-
-
-    void Awake() {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
-        g = new Vector3(0,0,0);
-    }
-    
-    private void FixedUpdate() {
-        move();
-    }
-
-    private void Update() {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * .5f + 0.2f, whatIsGround);
-        getInputs();
-    }
-
-    private void getInputs() {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticleInput = Input.GetAxisRaw("Vertical");
-    } 
-
-    private void move() {
-
-        if(grounded) {
-            rb.drag = groundDrag;
-            g.y = 0;
-        } else {
-            rb.drag = 0;
-            g.y -= gravityStrength;
-        }
-
-        movement = tf.forward * verticleInput + tf.right * horizontalInput;
-        rb.AddForce(movement.normalized * speed * 10f, ForceMode.Force);
-
-        rb.AddForce(g, ForceMode.Force);
-        Debug.Log(grounded);
-
-    }
-}
-*/
